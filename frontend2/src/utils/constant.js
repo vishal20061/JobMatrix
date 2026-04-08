9d7ecc266ffc9bfab3784da592e576d13dd9e471
Vishal Babu <vishaldiw4321@gmail.com>
2026-04-08 23:12:59 +0530

    for development Process

--- frontend2/src/utils/constant.js
+++ frontend2/src/utils/constant.js
@@ -1,5 +1,6 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+const API_URL = import.meta.env.VITE_API_URL;
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
@@ -7,7 +8,7 @@ export function cn(...inputs) {
 
 export const BASE_URL = "http://localhost:8000";
 
-export const USER_API_END_POINT = `${BASE_URL}/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/jobs`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const USER_API_END_POINT = `${API_URL}/user`;
+export const JOB_API_END_POINT = `${API_URL}/jobs`;
+export const APPLICATION_API_END_POINT = `${API_URL}/application`;
+export const COMPANY_API_END_POINT = `${API_URL}/company`;
